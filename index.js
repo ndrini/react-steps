214e8cc8b54a5053eb82667af3010e43e8671586
ndrini <ndrini@users.noreply.github.com>
2022-11-02 14:48:38 +0100

    feat: properly update the state

--- index.js
+++ index.js
@@ -2,66 +2,54 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-console.log("Let's start!");
+// import the possible steps
+const steps = require('./steps.json');
+// console.log(steps);
 
-const data = require('./steps.json');
-console.log(data);
-
+/**
+ * Provide the main page Component that shows
+ * description and the choices
+ *
+ * "step" it actual step, and defines the page to show by the code (of type string)
+ */
 function Step() {
-  // step defines the page to show by the code (of type string)
   const [step, setStep] = useState('0');
+  console.log('_________ step', step);
 
-  console.log(`the actual step ${step}`);
-
-  console.log(`the state ${data.steps}`);
-
-
-  let found = data.steps.find(function (element) {
+  // obtain the full object for actual step
+  let step_obj = steps.find(function (element) {
     return element.code == step;
   });
 
-  // Printing desired values.
-  console.log("found object (it should be just one!!!)", found);
+  console.log('_______________ step_obj', step_obj);
 
-  // console.log(data.filter(obj => {return obj.code === step}))
-  // console.log(data.find(function (x) {x.code === step}))
+  function handleChoice(code) {
+    // console.log('handleChoice code value: ', code);
+    setStep(code);
+  }
 
-  // function updateStep () { setStep } (update step)
-
-  // setEffect (update page on press button)
+  function ChoiceButton(props) {
+    return (
+      <button className="item" onClick={() => handleChoice(props.code)}>
+        {props.code} {props.description}
+      </button>
+    );
+  }
 
   return (
     <div>
-      The page content
-      {data.steps[0].title}
-      <button className="item"> Choice 1</button>
-      <button className="item"> Choice 2</button>
+      <h1>Title: {step_obj.title}</h1>
+      <p>Description: {step_obj.description}</p>
+
+      {step_obj.choices.map((singleChoice) => (
+        <ChoiceButton
+          description={singleChoice.description}
+          code={singleChoice.code}
+        />
+      ))}
     </div>
   );
 }
 
-/*
-function AddForm() {
-  const [sum, setSum] = useState(0);
-  const [num, setNum] = useState(0);
-
-  function handleChange(e) {
-    setNum(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    setSum(sum + Number(num));
-    e.preventDefault();
-  }
-
-  return <form onSubmit={handleSubmit}>
-  <Step />
-  <input type="number" value={num} onChange={handleChange} />
-  <input type="submit" value="Add" />
-  <p> Sum is {sum} </p>
-  </form>;
-} */
-
-// const el = <AddForm />;
 const el = <Step />;
 ReactDOM.render(el, document.getElementById('root'));

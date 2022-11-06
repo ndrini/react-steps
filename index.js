6d2376d82af5d9f0ddffc46520e97fe5579ec574
ndrini <ndrini@users.noreply.github.com>
2022-11-06 14:47:32 +0100

    feat: error importing yml file

--- index.js
+++ index.js
@@ -1,66 +1,30 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+const yaml = require('js-yaml');
+const fs = require('fs');
+
 import './style.css';
 
 // import the possible steps
 const steps = require('./steps.json');
-// console.log(steps);
+// const steps = require('./steps.yml');
+// const steps = yaml.load(fs.readFileSync('steps.yaml', 'utf8'));
 
-/**
- * Provide the main page Component that shows
- * description and the choices
- *
- * "step" it actual step, and defines the page to show by the code (of type string)
- */
-function Step() {
-  const [step, setStep] = useState('0');
-  console.log('_________ step', step);
+try {
+  let fileContents = fs.readFileSync('./steps.yml', 'utf8');
+  let data = yaml.safeLoad(fileContents);
 
-  // obtain the full object for actual step
-  let step_obj = steps.find(function (element) {
-    return element.code == step;
-  });
-
-  console.log('_______________ step_obj', step_obj);
-
-  function handleChoice(code) {
-    // console.log('handleChoice code value: ', code);
-    setStep(code);
-  }
-
-  function ChoiceButton(props) {
-    return (
-      <button className="button_green" onClick={() => handleChoice(props.code)}>
-        {props.code} {props.text_button}
-      </button>
-    );
-  }
-
-  function BackButton(props) {
-    return (
-      <button className="button_blue" onClick={() => handleChoice(props.code)}>
-        {props.code} {props.text_button}
-      </button>
-    );
-  }
-
-  return (
-    <div>
-      <h1>Title: {step_obj.title}</h1>
-      <img src={step_obj.image_path} />
-      <p>Description: {step_obj.description}</p>
-
-      {step_obj.choices.map((singleChoice) => (
-        <ChoiceButton
-          text_button={singleChoice.text_button}
-          code={singleChoice.code}
-        />
-      ))}
-
-      {step !== '0' ? <BackButton text_button="start again" code="0" /> : null}
-    </div>
-  );
+  console.log(data);
+} catch (e) {
+  console.log(e);
 }
 
-const el = <Step />;
-ReactDOM.render(el, document.getElementById('root'));
+console.log('bibbi');
+
+/*
+fs.readFile('./steps__.yml', (err, data) => {
+  if (err) throw err;
+
+  console.log(data.toString());
+});
+*/
